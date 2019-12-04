73db5cad36c0264136aaa88109e0db0cb4469b0e
robmalec <robmalec@umich.edu>
2019-12-04 16:01:20 -0500

    WListEntry now has constructor for MovieFullDeets

--- src/main/java/co/grandcircus/SpringAPIProject/repos/WListEntry.java
+++ src/main/java/co/grandcircus/SpringAPIProject/repos/WListEntry.java
@@ -4,26 +4,58 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import co.grandcircus.SpringAPIProject.APIEntitys.MovieFullDeets;
+import co.grandcircus.SpringAPIProject.APIEntitys.MoviePartDeets;
 
 @Entity
+@Table(name="MovieWatchlist")
 public class WListEntry {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-
 	private String title;
-	private String genre;
+	private String overview;
 	private int year;
+	
+	private int movie_id;
 
-	public WListEntry() {
-	}
-
-	public WListEntry(String title, String genre, int year) {
+	public WListEntry() {}
+	
+	public WListEntry(String title, String overview, int year, int movie_id) {
 		super();
 		this.title = title;
-		this.genre = genre;
+		this.overview = overview;
 		this.year = year;
+		this.movie_id = movie_id;
+	}
+	
+	public WListEntry(MovieFullDeets movie) {
+		super();
+		this.title = movie.getTitle();
+		this.overview = movie.getOverview();
+		String releaseDate = movie.getRelease_date();
+		this.year = Integer.valueOf(releaseDate.substring(0,4));
+		this.movie_id = movie.getId();
+	}
+
+	public WListEntry(Integer id, String title, String overview, int year, int movie_id) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.overview = overview;
+		this.year = year;
+		this.movie_id = movie_id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	public String getTitle() {
@@ -34,12 +66,12 @@ public class WListEntry {
 		this.title = title;
 	}
 
-	public String getGenre() {
-		return genre;
+	public String getOverview() {
+		return overview;
 	}
 
-	public void setGenre(String genre) {
-		this.genre = genre;
+	public void setOverview(String overview) {
+		this.overview = overview;
 	}
 
 	public int getYear() {
@@ -50,4 +82,17 @@ public class WListEntry {
 		this.year = year;
 	}
 
+	public int getMovie_id() {
+		return movie_id;
+	}
+
+	public void setMovie_id(int movie_id) {
+		this.movie_id = movie_id;
+	}
+
+	@Override
+	public String toString() {
+		return "WListEntry [id=" + id + ", title=" + title + ", overview=" + overview + ", year=" + year + ", movie_id="
+				+ movie_id + "]";
+	}
 }
