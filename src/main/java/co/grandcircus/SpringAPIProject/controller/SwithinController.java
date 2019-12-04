c37316aa23fcacdf3e3e22615800f6e5f912c255
SwithinJoseph <55331152+SwithinJoseph@users.noreply.github.com>
2019-12-04 15:52:14 -0500

    ..

--- src/main/java/co/grandcircus/SpringAPIProject/controller/SwithinController.java
+++ src/main/java/co/grandcircus/SpringAPIProject/controller/SwithinController.java
@@ -1,11 +1,6 @@
 package co.grandcircus.SpringAPIProject.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import co.grandcircus.SpringAPIProject.repos.WListRepo;
 
 @Controller
 public class SwithinController {
